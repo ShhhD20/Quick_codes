bae9937d7a26c222bc99b63fb68724c25374f6f6
ShhhD20 <142339741+ShhhD20@users.noreply.github.com>
2023-10-05 12:37:31 -0600

    Add files via upload

--- Nombres.cpp
+++ Nombres.cpp
@@ -5,15 +5,18 @@
 int main (){
 	int ans = 11;
 	int A[10] = {11, 11, 11, 11, 11, 11, 11, 11, 11, 11};
-	char Name[10][10] = {"Carlitos\n", "Vero\n", "Quique\n", "Susy\n", "Fran\n", "Majo\n", "Toño\n", "Bibi\n", "Chema\n", "Caro\n"};
+	char Agua[12][10] = {"Feraligatr\n", "Tentacool\n", "Empoleon\n", "Magikarp\n", "Omanyte\n", "Poliwrath\n", "Starmie\n", "Lapras\n", "Wooper\n", "Squirtle\n"};
+	char Fuego[12][10] = {"Torkoal\n", "Chandelure\n", "Charizard\n", "Darmanitan\n", "Talonflame\n", "Incineroar\n", "Houndoom\n", "Flareon\n", "Ninetales\n", "Ponyta\n"};
 	srand(time(NULL));
 	
-	for(int a = 0; a < 10; a++){
-		while(ans == A[1] || ans == A[2] || ans == A[3] || ans == A[4] || ans == A[5] || ans == A[6] || ans == A[7] || ans == A[8] || ans == A[9] || ans == A[0]){
-		ans = rand() % 10;
+	for(int b = 0; b < 1; b++){
+		for(int a = 0; a < 10; a++){
+			while(ans == A[1] || ans == A[2] || ans == A[3] || ans == A[4] || ans == A[5] || ans == A[6] || ans == A[7] || ans == A[8] || ans == A[9] || ans == A[0]){
+			ans = rand() % 10;
+			}
+			A[a] = ans;
+			printf("%s", Name[ans]);
 		}
-		A[a] = ans;
-		printf("%s", Name[ans]);
 	}
 	return 0;
 }
